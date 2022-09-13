1053ee9e5b59522a5178b122d961a46197e1db0f
Sourabh Choure <sourabhchoure98@gmail.com>
2022-09-13 14:21:10 -0500

    added frontend for clients

---
+++ managerfrontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Box from '@mui/material/Box';
+import Login from './LoginPage/Login';
+
+function App() {
+  return (
+    <Box flexGrow={1}>
+      <Login/>
+    </Box>
+  );
+}
+
+export default App;
